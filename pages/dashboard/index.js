c787793bf6cca1bfec8e37ab1885667e8f24c08d
michaelmichio <michaelmichio.work@gmail.com>
2023-05-29 15:35:41 +0700

    update 29 mei 2023 9

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -174,7 +174,7 @@ export default function DashboardIndex(props) {
                         (orders.printCount < 1 ?
                           <span className="px-2 py-1 font-semibold leading-tight text-amber-700 bg-amber-100 rounded-full dark:bg-amber-700 dark:text-amber-100">Belum Selesai</span>
                           :
-                          <span className="px-2 py-1 font-semibold leading-tight text-amber-700 bg-amber-100 rounded-full dark:bg-amber-700 dark:text-amber-100">Belum Dicetak</span>
+                          <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">Selesai</span>
                           )
                         :
                         <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">Sudah Dicetak</span>
